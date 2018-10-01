9e9fbd30c15730d507d04dbe510c6d50c20a0679
ashdham10 <ashleydenham10@gmail.com>
2018-10-01 13:03:15 -0500

    finished

--- app.js
+++ app.js
@@ -11,11 +11,23 @@ class Die {
        container.appendChild(this.die);
        this.die.innerText = value;
        this.roll();
+       this.oneRoll();
+       this.removeDie();
     } 
     roll(){
         let rollButton = document.getElementById('roll-dice');
-        rollButton.addEventListener('click', () =>{
+        rollButton.addEventListener('click', () => {
             this.die.innerText = randomNum();
+        });
+    }
+    oneRoll(){
+        this.die.addEventListener('click', () => {
+            this.die.innerText = randomNum();
+        });
+    }
+    removeDie(){
+        this.die.addEventListener('dblclick', () => {
+           container.removeChild(this.die);
         });
     }
 }
@@ -31,5 +43,24 @@ function insertDie(){
     let num = randomNum();
     let newDie = new Die(num);
 }
+
+let sumButton = document.getElementById('sum-dice');
+sumButton.addEventListener('click', getSum);
+
+
+ function getSum(){
+    let innerHTMLs = Array.prototype.slice.call(document.getElementsByClassName('dice')).map(function(x){return x.innerHTML}); 
+    let reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let arr = innerHTMLs.reduce(reducer).split('');
+    let arr2 = []
+    arr.forEach(element => {
+        let num = +element;
+        arr2.push(num);
+    });
+    let sum = arr2.reduce((a, b) => a + b, 0);
+
+    alert(`Sum of dice is ${sum}!`);
+};
+
     
 });
