51aaac981e0821d80361843c7f4ba528068684bd
ashdham10 <ashleydenham10@gmail.com>
2018-10-01 08:56:35 -0500

    done

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
     console.log('dom content loaded!');
 
-class Die {
-    constructor(value){
-        this.value = value;
-    }
-    roll(){
-        this.value = Math.floor((Math.random() * 6) + 1);
-    }
-};
-
 let container = document.createElement('div');
 document.body.appendChild(container);
 
-document.getElementById('getDie').onclick = function(e){
-    let die = document.createElement('div');
-    die.classList.add('dice');
-    container.appendChild(die);
-    
+class Die {
+    constructor(value){
+       this.die = document.createElement('div');
+       this.die.classList.add('dice');
+       container.appendChild(this.die);
+       this.die.innerText = value;
+       this.roll();
+    } 
+    roll(){
+        let rollButton = document.getElementById('roll-dice');
+        rollButton.addEventListener('click', () =>{
+            this.die.innerText = randomNum();
+        });
+    }
 }
 
+function randomNum(){
+    return Math.floor(Math.random() * 6) + 1;
+}
 
+let dieButton = document.getElementById('get-die');
+dieButton.addEventListener('click', insertDie);
 
+function insertDie(){
+    let num = randomNum();
+    let newDie = new Die(num);
+}
+    
 });
